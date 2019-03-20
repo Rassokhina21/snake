16dc3fdb54602dc93e129bce3118202b7bce68a2
Lena <erassokhina@intervale.ru>
2019-03-20 15:50:21 +0300

    first
    Signed-off-by: Lena <erassokhina@intervale.ru>

---
+++ ConsoleApplication1/ConsoleApplication1/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
+
+
+Namespace Snake
+    {
+    Class Program
+        {
+            static void Main( string[] args )
+            {
+                Console.WriteLine("Hello world");
+                Console.Readline();
+            }
+        }
+    }
+    End Class
+End Namespace
