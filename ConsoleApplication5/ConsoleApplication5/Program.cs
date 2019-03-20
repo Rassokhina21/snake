95dfc41b3c97a46a46d9339c6ee774e47bf22e39
Lena <lekalarina@mail.ru>
2019-03-20 16:56:52 +0300

    4 ИНКАПСУЛЯЦИЯ
    Signed-off-by: Lena <lekalarina@mail.ru>

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 2, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            Console.ReadLine(); 
+       
+        }
+    }
+}
